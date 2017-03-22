6bb9214e0636bfb257127539d54ad8dabb37cb56
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-03-22 15:03:29 +0200

    Button component description

--- src/docs/button.page.js
+++ src/docs/button.page.js
@@ -6,17 +6,27 @@ class ButtonPage extends Component {
     return(
       <div>
         <h1>Button Component</h1>
-        <h2>Button types</h2>
+        <h2>Button types example</h2>
         <table>
           <tr>
             <td><UIButton type="primary">Primary</UIButton></td>
             <td><UIButton type="primary" disabled={true}>Primary disabled</UIButton></td>
             <td><UIButton type="primary cancel">Primary Cancel</UIButton></td>
+          </tr>
+          <tr>
             <td><UIButton type="secondary">Secondary</UIButton></td>
             <td><UIButton type="secondary" disabled={true}>Secondary disabled</UIButton></td>
             <td><UIButton type="secondary cancel">Secondary cancel</UIButton></td>
           </tr>
         </table>
+        <h2>Using inside project</h2>
+        <pre>
+          <code>
+            &lt;UIButton type="primary | secondary" disabled=&#123;true | false&#125; &gt;&lt;/UIButton&gt;
+          </code>
+        </pre>
+        <p><strong>type</strong> - Required</p>
+        <p><strong>disabled</strong> - Not Required</p>
       </div>
     )
   }

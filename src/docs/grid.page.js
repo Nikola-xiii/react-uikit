f1e4ff7ad258a16a3375915b717cf01bfbb19251
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-03-23 17:41:51 +0200

    Added doc for grid component

--- src/docs/grid.page.js
+++ src/docs/grid.page.js
@@ -86,40 +86,94 @@ class GridPage extends Component {
       <div>
         <h1>Grid component</h1>
         <h2>Grid view</h2>
+        <h2>Using inside project use Row Structure</h2>
+        <pre className="language-markup">
+          <code className="language-markup">
+            {`<UIGrid gridDataRows={GridDataRows} settings={Settings}></UIGrid>`}
+          </code>
+        </pre>
         <UIGrid gridDataRows={GridDataRows} settings={Settings}/>
-        <h2>Using inside project</h2>
-        <code>&lt;UIGrid gridDataRows=&#123;GridDataRows&#125; settings=&#123;Settings&#125;&gt;&lt;/UIGrid&gt;</code>
+        <h3>Data structure settings</h3>
         <pre className="language-javascript">
           <code className="language-javascript">
-            const GridDataRows = [
-              &#123;
-              id: 11,
-              name: 'Test1',
-              email: 'test1@test.com',
-              vendor: 'LoopMe',
-              company: 'Home',
-              action: 'input'
-              &#125;,
-              ...
-            ];
+            {`const GridDataRows = [
+  {
+    id: 11,
+    name: 'Test1',
+    email: 'test1@test.com',
+    vendor: 'LoopMe',
+    company: 'Home',
+    action: {
+      name: 'Login',
+      icon: 'input'
+    }
+  },
+  ...
+  ]`}
           </code>
         </pre>
         <h3>Grid settings</h3>
         <pre className="language-javascript">
           <code className="language-javascript">
-            const Settings = &#123;
-            headers: [
-            &#123; key: 'id', name: 'id', sortable: true, type: 'text'&#125;,
-            &#123; key: 'name', name: 'name', sortable: false, type: 'text'&#125;,
-            &#123; key: 'company', name: 'company', sortable: false, type: 'text'&#125;,
-            &#123; key: 'vendor', name: 'vendor', sortable: true, type: 'text'&#125;,
-            &#123; key: 'email', name: 'email', sortable: false, type: 'link'&#125;,
-            &#123; key: 'action', name: '', sortable: false, type: 'action'&#125;,
-              ],
-            &#125;;
+            {`const Settings = {
+  headers: [
+    { key: 'id', name: 'id', sortable: true, type: 'text'},
+    { key: 'name', name: 'name', sortable: false, type: 'text'},
+    { key: 'company', name: 'company', sortable: false, type: 'text'},
+    { key: 'vendor', name: 'vendor', sortable: true, type: 'text'},
+    { key: 'email', name: 'email', sortable: false, type: 'link'},
+    { key: 'action', name: '', sortable: false, type: 'action'},
+  ],
+};`}
+          </code>
+        </pre>
+        <h2>Using inside project use Column Structure</h2>
+        <pre className="language-markup">
+          <code className="language-markup">
+            {`<UIGrid gridDataRows={GridDataColumns} settings={Settings}></UIGrid>`}
           </code>
         </pre>
         <UIGrid gridDataColumns={GridDataColumns} settings={Settings}/>
+        <h3>Data structure settings</h3>
+        <pre className="language-javascript">
+          <code className="language-javascript">
+            {`const GridDataColumns = {
+  id: [11, 12, 13, 14],
+  name: ['Test1', 'Test2', 'Test3', 'Test4'],
+  email: ['test1@test.com', 'test2@test.com', 'test3@test.com', 'test4@test.com'],
+  vendor: ['LoopMe', '', 'EA', 'LoopMe'],
+  company: ['LoopMe', 'LoopMe', 'LoopMe', 'LoopMe'],
+  action: [{
+    name: 'Login',
+    icon: 'input'
+  }, {
+    name: 'Login',
+    icon: 'input'
+  }, {
+    name: 'Login',
+    icon: 'input'
+  }, {
+    name: 'Login',
+    icon: 'input'
+  }]
+};`}
+          </code>
+        </pre>
+        <h3>Grid settings</h3>
+        <pre className="language-javascript">
+          <code className="language-javascript">
+            {`const Settings = {
+  headers: [
+    { key: 'id', name: 'id', sortable: true, type: 'text'},
+    { key: 'name', name: 'name', sortable: false, type: 'text'},
+    { key: 'company', name: 'company', sortable: false, type: 'text'},
+    { key: 'vendor', name: 'vendor', sortable: true, type: 'text'},
+    { key: 'email', name: 'email', sortable: false, type: 'link'},
+    { key: 'action', name: '', sortable: false, type: 'action'},
+  ],
+};`}
+          </code>
+        </pre>
       </div>
     )
   }
